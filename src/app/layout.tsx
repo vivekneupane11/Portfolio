8a242d0ebb4f78bc5b001ba9a3fd6f6b5b9c3639
vivekneupane11 <vivekneupane11@gmail.com>
2023-03-28 14:22:47 +0545

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,30 @@
 import ThemeProviders from '@/ThemeProviders'
 import Header from './components/Header'
 import './globals.css'
-
+import {Roboto} from '@next/font/google'
 export const metadata = {
   title: 'Vivek Neupane',
   description: 'Vivek Neupane(Chief Technology Officer) of Medicos International. I am a software engineer and creator.',
 }
 
+const roboto = Roboto({
+  subsets:['latin'],
+  weight:['400','700']
+});
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={roboto.className}>
+      <body className='dark:bg-darkSecondary'>
      <ThemeProviders>
-     <Header/>
-        {children}
+<main className=''>
+  <h1>Left SideBar</h1>
+{children}
+
+</main>
      </ThemeProviders>
         </body>
     </html>
